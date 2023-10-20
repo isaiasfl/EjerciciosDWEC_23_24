2c38e6324d92e0e0229f5a658da02bc20ce835ca
PckProgramador <958patrick@gmail.com>
2023-10-20 20:06:53 +0200

    commit confirmaicion

--- ejercicio4Relacion3/ejercicio4relacion3.js
+++ ejercicio4Relacion3/ejercicio4relacion3.js
@@ -1,3 +1,6 @@
+
+
+// Author : PckProgramador
 //Arrow function que crea un objeto a partir de una cadena que va a ser 
 //el valor de la propiedad. Hay que poner los parentesis para que diferencie
 // que es un objeto y no una apertura de llaves estandar.
