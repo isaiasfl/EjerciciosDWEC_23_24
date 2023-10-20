bd3b632ff37d3b568eefaba69bc367ddfd96fbf4
PckProgramador <958patrick@gmail.com>
2023-10-20 19:59:10 +0200

    ejercicio 4 relacion 3 objetos Patrick

---
+++ ejercicio4Relacion3/ejercicio4relacion3.js
@@ -0,0 +1,9 @@
+//Arrow function que crea un objeto a partir de una cadena que va a ser 
+//el valor de la propiedad. Hay que poner los parentesis para que diferencie
+// que es un objeto y no una apertura de llaves estandar.
+
+const objeto4 = cadena => ({"key":cadena}); 
+
+//devuleve un objeto,para crear un objeto
+// const objetoDevuelto = objeto4("hola");
+//Devuelve {key:"hola"};
