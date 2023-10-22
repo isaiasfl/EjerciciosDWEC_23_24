e76cd8782ca4ca21e517fe6ae77ad4caff0c3bf9
aronaldo9 <104202541+aronaldo9@users.noreply.github.com>
2023-10-22 20:48:38 +0200

    Añadida documentación a modules.js

--- ejerc2Prototypes/assets/modules.js
+++ ejerc2Prototypes/assets/modules.js
@@ -21,7 +21,10 @@ export const ComidaLiteral = {
         }        
     },
 
-    // Método que devuelve la cantidad de calorías
+    /**
+     * Calcula la cantidad de calorías de la comida.
+     * @returns {number} La cantidad de calorías de la comida.
+     */
     calcularCalorias() {
         return this._calorias;
     }
@@ -31,15 +34,27 @@ export const ComidaLiteral = {
 export const DietaLiteral = {
     _alimentosConsumidos : [],
 
+    /**
+     * Agrega un objeto de tipo Comida al arreglo de alimentos consumidos.
+     * @param {object} newComida - El objeto de tipo Comida que se va a agregar.
+     */
     agregarAlimento(newComida) {
         this._alimentosConsumidos.push(newComida);
     },
 
+    /**
+     * Calcula el total de calorías consumidas en la dieta.
+     * @returns {number} El total de calorías consumidas.
+     */
     calcularTotalCalorias() {
         const totalCalorias = this._alimentosConsumidos.reduce( (acc,comida) => acc + comida._calorias,0);
         return totalCalorias;
     },
 
+    /**
+     * Lista los alimentos consumidos en la dieta junto con sus calorías.
+     * @returns {string[]} Un array de cadenas con el nombre y las calorías de cada alimento consumido.
+     */
     listarAlimentosConsumidos() {
         console.log( this._alimentosConsumidos.map(comida => `Comida: ${comida._nombre}, Calorías: ${comida._calorias}`)); 
         return this._alimentosConsumidos.map(comida => `Comida: ${comida._nombre}, Calorías: ${comida._calorias}`);
@@ -74,7 +89,11 @@ export function ComidaFuncional(nombre,calorias) {
             } 
         },
     });
-    
+
+    /**
+     * Calcula la cantidad de calorías de la comida.
+     * @returns {number} La cantidad de calorías de la comida.
+     */
     ComidaFuncional.prototype.calcularCalorias = function () {
         return this.calorias;
     };
@@ -84,16 +103,28 @@ export function ComidaFuncional(nombre,calorias) {
 export function DietaFuncional() {
     this._alimentosConsumidos = [];
 
+    /**
+     * Agrega un objeto de tipo Comida a la dieta.
+     * @param {object} comida - El objeto de tipo Comida que se va a agregar a la dieta.
+     */
     DietaFuncional.prototype.agregarAlimento = function (comida) {
         this.alimentosConsumidos.push(comida);
     };
 
+    /**
+     * Calcula el total de calorías consumidas en la dieta.
+     * @returns {number} El total de calorías consumidas.
+     */
     DietaFuncional.prototype.calcularTotalCalorias = function () {
         return this.alimentosConsumidos.reduce(function (acc, comida) {
             return acc + comida.calcularCalorias();
         }, 0);
     };
 
+    /**
+     * Lista los alimentos consumidos en la dieta junto con sus calorías.
+     * @returns {string[]} Un array de cadenas con el nombre y las calorías de cada alimento consumido.
+     */
     DietaFuncional.prototype.listarAlimentosConsumidos = function () {
         return this._alimentosConsumidos.map(comida => `Comida: ${comida._nombre}, Calorías: ${comida._calorias}`);
     };
@@ -123,6 +154,11 @@ class ComidaClase {
             console.log("La cantidad de calorías debe ser mayor que 0");
         } 
     }
+    
+    /**
+     * Calcula la cantidad de calorías de la comida.
+     * @returns {number} La cantidad de calorías de la comida.
+     */    
     calcularCalorias() {
         return this._calorias;
     }
@@ -133,14 +169,25 @@ class DietaClase {
         this.alimentosConsumidos = [];
     }
 
+    /**
+     * Agrega un objeto de tipo Comida a la dieta.
+     * @param {object} comida - El objeto de tipo Comida que se va a agregar a la dieta.
+     */
     agregarAlimento(comida) {
         this.alimentosConsumidos.push(comida);
     }
 
+    /**
+     * Calcula el total de calorías consumidas en la dieta.
+     * @returns {number} El total de calorías consumidas.
+     */
     calcularTotalCalorias() {
         return this.alimentosConsumidos.reduce((acc, comida) => acc + comida.calcularCalorias(), 0);
     }
 
+    /**
+     * Lista los alimentos consumidos en la dieta junto con sus calorías.
+     */
     listarAlimentosConsumidos() {
         this.alimentosConsumidos.forEach(comida => {
             console.log(`Comida: ${comida.nombre}, Calorías: ${comida.calcularCalorias()}`);
