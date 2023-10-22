9b793fb0f0579f14d2f52e3687a35cec13ae3992
PepeRivera04 <peperivera1fpb@gmail.com>
2023-10-22 19:44:46 +0200

    Añadida descripcion de la funcion en estructura de comentarios

--- Relaciones/JavaScript/Relacion2_Arrays/ej24/ejercicio24.js
+++ Relaciones/JavaScript/Relacion2_Arrays/ej24/ejercicio24.js
@@ -1,16 +1,16 @@
 /**
  * @author: Jose Antonio Rivera Bandera
  * @description: Escribe una función llamada ordenarPorPropiedad que ordene un array de objetos por una
-propiedad específica. La función debe tomar un array arr y una cadena propiedad que
-indique la propiedad por la cual se debe ordenar.
+ * propiedad específica. La función debe tomar un array arr y una cadena propiedad que
+ *  indique la propiedad por la cual se debe ordenar.
  */
 
 /**
+ * Función que recibe un array de objetos y una cadena, devolverá el array de objetos ordenados por la propiedad cadena
  * @param {Array} arr
- * @param {string} propiedad
+ * @param {String} propiedad
  * @returns Array
  */
-
 const ordenarPorPropiedad = (arr, propiedad) =>
   // Dentro de los parametros de sort() evalua si la propiedad es mayor o menor que la del resto de objetos
   // en función del resultado se devolvera 1 para mayor , -1 para menor y 0 para iguales, lo cual hará que el sort las ordene.
