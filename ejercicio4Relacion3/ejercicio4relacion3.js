28ed91c3449654568b064ab4b9a54dc9874ed8bd
PckProgramador <958patrick@gmail.com>
2023-10-22 19:39:15 +0200

    añado descripcion a las funciones

--- ejercicio4Relacion3/ejercicio4relacion3.js
+++ ejercicio4Relacion3/ejercicio4relacion3.js
@@ -2,14 +2,14 @@
 
 // @Author : Patrick Moreno
 // @Description:Arrow function que crea un objeto a partir de una cadena que va a ser 
-//el valor de la propiedad. Hay que poner los parentesis para que diferencie
-// que es un objeto y no una apertura de llaves estandar.
-
+//el valor de la propiedad. 
 
 
 //Ejemplo 1 return implicito
 /**
- * 
+ * Description:funcion que crea un objeto a partir de un String "cadena" que va a ser 
+ * el valor de la propiedad "key". Usamos un return implicito 
+ * Hay que poner los parentesis para que diferencie que es un objeto y no una apertura de llaves estandar.
  * @param {String} cadena 
  * @returns Object
  */
@@ -18,12 +18,17 @@ const objeto4 = cadena => ({"key":cadena});
 
 //Ejemplo 2 arrow function
 /**
- * 
+ * Description:funcion que crea un objeto a partir de un String "cadena" que va a ser 
+ * el valor de la propiedad "key". Usamos un return estandar
  * @param {String} cadena 
  * @returns Object
  */
 const objeto42= cadena => { return {"key":cadena}};
 
-//devuleve un objeto,para crear un objeto
-// const objetoDevuelto = objeto4("hola");
-//Devuelve {key:"hola"};
+/** EJEMPLO DE USO
+ *  const objetoDevuelto = objeto4("hola");
+ *  Devuelve {key:"hola"};
+ * 
+ *  const objetoDevuelto = objeto42("hola");
+ *  Devuelve {key:"hola"};
+ */
