b8f4da171b2db067b2a92de0db2c62dce3ca160d
NuMeRo-999 <pedrovp003@gmail.com>
2023-10-24 12:14:48 +0200

    Mejora de la documentación del Ejercicio10 de Objetos

--- Relaciones/JavaScript/Relacion3_Objetos/Ejercicio10/Ejercicio10.js
+++ Relaciones/JavaScript/Relacion3_Objetos/Ejercicio10/Ejercicio10.js
@@ -12,13 +12,15 @@ objeto = {
 
 /**
  * @description: Función que toma como argumento un objeto y una propiedad, elimina la propiedad pasada como agumento del objeto pasado y devuelve dicho objeto.
- * @param {Object} objeto 
- * @param {string} propiedad 
- * @returns {Object}
+ * @param {Object} objeto - objeto al cual se le va a elminar la clave
+ * @param {string} propiedad - clave que quieres eliminar
+ * @returns {Object} - objeto con la clave eliminada
  */
 function eliminarPropiedad(objeto, propiedad){
   delete objeto[propiedad];
   return objeto;
 }
+
+// --- test ---
 
 console.log(eliminarPropiedad(objeto, 'b')); // Borra la propiedad con la clave 'b' y muestra el resultado por consola.
