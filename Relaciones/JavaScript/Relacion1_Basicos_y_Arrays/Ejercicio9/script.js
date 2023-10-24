2c11d2ed3b9d4dcbefcd663f6ce826f7c2450970
Crixls <crixismundial@gmail.com>
2023-10-24 13:25:10 +0200

    Relación I Arrays. Ejercicio 9 (Correción)

--- Relaciones/JavaScript/Relacion1_Basicos_y_Arrays/Ejercicio9/script.js
+++ Relaciones/JavaScript/Relacion1_Basicos_y_Arrays/Ejercicio9/script.js
@@ -6,22 +6,23 @@
 
 /**
  * Función que devuelve el índice en el que se encuentra el elemento buscado dentro de un array
- * @param {Array} arr 
- * @param {Number} elemento 
- * @returns {Number}
+ * @param {Array} arr // Este es el array que se pide en la función
+ * @param {Number} elemento  // Este es elemento que estamos buscando en el array en este caso
+ * @returns {Number} // Este número es el índice de mi elemento encontrado
  */
 const buscarElemento = (arr,elemento) => {
-    let foundIndex = -1; // Al no encontrar el elemento se inicializa a -1
+    let foundIndex = -1; // "foundIndex" se inicializa a -1 porque aún no se ha encontrado el elemento
     
-    arr.forEach((valor, index) => {// Se recorren todos los elementos del array y junto a su índice
+    arr.forEach((valor, index) => {// Se recorren todos los elementos del array junto a su índice
         if (valor === elemento) {
-            foundIndex = index; // Actualizamos el índice si encontramos el elemento
+            foundIndex = index; // Actualizamos "foundIndex" si encontramos el elemento con el índice del elemento encontrado
         }
     });
     
      return foundIndex; // Devolvemos el índice del elemento encontrado o -1 si no se encontró
 }
 
+//------ Test -------
 
 const array= [3,5,7,2,1,5];
 const resultado=buscarElemento(array,2);
