f24df3e94c74be40cc2a9c7fa2074e41fba0b405
miguel470 <migueljesusortegagonzalez@gmail.com>
2023-10-24 18:42:22 +0200

    Ejercicio 7 arrays y ejercicio 14 objetos PR

---
+++ Relaciones/JavaScript/Relacion3_Objetos/Ejercicio14/ejercicio14OBJ.js
@@ -0,0 +1,37 @@
+/**
+ * @author: miguel470
+ * @description: Reemplazar cadenas vacías en el objeto con valores nulos. Escriba una función que tome un objeto
+ * como argumento.Algunos de los valores de propiedad contienen cadenas vacías. Reemplace cadenas
+ * vacías y cadenas que contienen solo espacios en blanco con valores nulos. Devuelva el objeto
+ * resultante.
+ */
+
+/**
+ * Las cadenas que esten vacías seran reemplazadas en un nuevo objeto por NULL-
+ * 
+ * @param {Object} - Pasamos un objeto para comprobar si tiene cadenas vacías
+ * @returns { Object } - Devuelve el objeto con las cadenas vacías cambiadas por NULL
+ */
+function eliminarNull(obj) {
+    for (const key in obj) {
+      // Sacamos el valor
+      if (obj.hasOwnProperty(key)) {
+        const valor = obj[key];
+        // Comprobamos si es un string y si esta vacio hacemos lo de dentro del if
+        if (typeof valor === 'string' && valor.trim() === '') {
+          obj[key] = null; // Reemplazamos la cadena vacía por null
+        }
+      }
+    }
+    return obj;
+  }
+
+//----test----
+
+  const objeto = {
+    nombre: 'Juan',
+    apellido: '',
+    direccion: '   ',
+    edad: 30,
+  };
+console.log(eliminarNull(objeto));
