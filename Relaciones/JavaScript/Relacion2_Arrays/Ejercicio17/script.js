2e03a7f408b9814986c909c36739634676e91df8
rjodomin22 <rjodomin@gmail.com>
2023-10-25 21:17:15 +0200

    final Ejercicio 17

--- Relaciones/JavaScript/Relacion2_Arrays/Ejercicio17/script.js
+++ Relaciones/JavaScript/Relacion2_Arrays/Ejercicio17/script.js
@@ -1,20 +1,39 @@
 /**
- * @author ....
- * @description xxxxxxx
+ * @author: Raisa Julia Ochoa Domínguez
+ *@description: Escribe una función llamada multiplicarMatrizPorEscalar que tome una matriz matriz y un número escalar.
+ *La función debe multiplicar cada elemento de la matriz por el escalar y devolver una nueva matriz con los
+ *resultados.
  */
 
 /**
- * Fucnión que calucladasdf asdf añsdf asdf
- * @param {String} a  Texto para cambiar
- * @param {Number} n  njmero de veces...
- * @returns {String} texto texto que es la isss
+ * Función con la que tomaremos una matriz y numero escalar que utilizaremos para multiplicar la matriz
+ * @param {[Array][Array]} matriz Matriz de numeros a multiplicar
+ * @param {number} escalar Nuemro por el que multiplicamos la matriz
+ * @returns Nueva matriz de numeros
  */
-function saludar(a, n) {
-  return "hola";
+function multiplicarMatrizPorEscalar(matriz, escalar) {
+  const resultado = [];
+
+  for (let i = 0; i < matriz.length; i++) {
+    const filaResultado = [];
+    for (let j = 0; j < matriz[i].length; j++) {
+      filaResultado.push(matriz[i][j] * escalar);
+    }
+
+    resultado.push(filaResultado);
+  }
+
+  return resultado;
 }
 
-//fasdfas
+//------------------- TEST ------------------
 
-// --------- test ----
+const matriz = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+let escalar = 2;
 
-saludar(sdafas, dasdfas);
+const matrizResultante = multiplicarMatrizPorEscalar(matriz, escalar);
+console.log(matrizResultante);
