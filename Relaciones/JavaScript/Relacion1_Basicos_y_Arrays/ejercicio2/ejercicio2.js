955e283c50f1c355fdc50eed8c776b0219ae1147
Ambrosio03 <ambrosio0304@gmail.com>
2023-10-25 12:49:16 +0200

    Comentario de la funcion

--- Relaciones/JavaScript/Relacion1_Basicos_y_Arrays/ejercicio2/ejercicio2.js
+++ Relaciones/JavaScript/Relacion1_Basicos_y_Arrays/ejercicio2/ejercicio2.js
@@ -1,18 +1,11 @@
 /**
  * @author: Ambrosio Iglesias Prieto
- * @description: Define una función llamada eliminarDuplicados que tome un array arr . La función debe
-eliminar los elementos duplicados y devolver un nuevo array con elementos únicos, manteniendo
-el orden original
- */
+ * @description: toma un array arrq elimina los elementos duplicados y devuelve un nuevo array con elementos únicos, manteniendo
+  el orden original
+* @param {arr} "es un array"
+* @returns array sin duplicados
+*/
 
-
-/**
- * La línea const eliminarDuplicados = (arr) => define una función llamada eliminarDuplicados que recibe un arreglo como parámetro.
-   La línea arr.filter((valor, indice, array) => llama al método filter() del objeto Array para recorrer cada elemento del arreglo original.
-   La línea array.indexOf(valor) === indice es la función callback que se llama para cada elemento del arreglo. Esta función devuelve true si 
- * el elemento actual no es un duplicado.
- *  arr = es el array que le pasamos
- */
 const eliminarDuplicados = (arr) =>  arr.filter((valor, indice, array) => array.indexOf(valor) === indice);
   
   
