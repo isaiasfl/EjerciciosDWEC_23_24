652b3f240fdc81e9bf365ba8e5c73723459f378b
rjodomin22 <rjodomin@gmail.com>
2023-10-25 21:01:09 +0200

    Ejercicio 17

---
+++ Relaciones/JavaScript/Relacion2_Arrays/Ejercicio17/script.js
@@ -0,0 +1,18 @@
+/**
+ * @author ....
+ * @description xxxxxxx
+ */
+
+/**
+ * Fucnión que calucladasdf asdf añsdf asdf
+ * @param {String} a  Texto para cambiar
+ * @param {Number} n  njmero de veces...
+ * @returns {String} texto texto que es la isss
+ */
+function saludar(a, n) {
+  return "hola";
+}
+
+// --------- test ----
+
+saludar(sdafas, dasdfas);
