793d08fd74e974c2d3343f709210ad82e325cf13
AbelMP17 <abelmp890@gmail.com>
2023-10-25 19:31:35 +0200

    Ejercicios 3 y 7 de la 3ª y 4ª relacion de ejercicios

--- Ejercicios3y7/Ejercicio3.js
+++
@@ -1,9 +0,0 @@
-/**
- * @author: Abel Martinez Peinado
- * @description: Escribe una función que tome un objeto (a) y una cadena (b) como argumento. Devuelve verdadero si el objeto tiene una propiedad con la clave 'b', pero solo si tiene un valor verdadero. En otras palabras, no debe ser nulo, indefinido o falso. Devuelve falso en caso contrario.
- * @version: 1.0
-*/
-
-const tienePropiedadVerdadera = (objeto, clave) => objeto.hasOwnProperty(clave) && objeto[clave] === true ? true : false;
-
-console.log(tienePropiedadVerdadera({a: 'alo', b: false, c: true}, 'c'));
