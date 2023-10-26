ca94ad54f7a387e89f84ec19642ff12258da4709
amp18 <95914401+AbelMP17@users.noreply.github.com>
2023-10-26 10:26:50 +0200

    Comentando Ejercicio 3 Objetos

--- Relaciones/JavaScript/Relacion3_Objetos/Ejercicio3.js
+++ Relaciones/JavaScript/Relacion3_Objetos/Ejercicio3.js
@@ -4,6 +4,15 @@
  * @version: 1.0
 */
 
+/**
+ * Esta función mediante el método hasOwnProperty comprueba que el objeto contenga la propiedad especificada
+ * y luego se comprueba si dicha clave contiene un valor true.
+ *
+ * @param {Object} objeto - Objeto el cual contenga una propiedad con la clave
+ * @param {*} clave - esta clave es la propiedad que debe contener el objeto
+ * return Boolean
+ */
 const tienePropiedadVerdadera = (objeto, clave) => objeto.hasOwnProperty(clave) && objeto[clave] === true ? true : false;
 
+//Prueba donde inserto un objeto que contenga la propiedad c y esta sea true.
 console.log(tienePropiedadVerdadera({a: 'alo', b: false, c: true}, 'c'));
