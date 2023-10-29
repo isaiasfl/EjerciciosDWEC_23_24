217c2d4950689a2ead53896d78f09b58d36aa5eb
rjodomin22 <rjodomin@gmail.com>
2023-10-29 21:40:53 +0100

    update Ejercicio 17

--- Relaciones/JavaScript/Relacion2_Arrays/Ejercicio17/script.js
+++ Relaciones/JavaScript/Relacion2_Arrays/Ejercicio17/script.js
@@ -7,20 +7,44 @@
 
 /**
  * Función con la que tomaremos una matriz y numero escalar que utilizaremos para multiplicar la matriz
- * @param {[Array][Array]} matriz Matriz de numeros a multiplicar
+ * @param {[Array]} matriz Matriz de numeros a multiplicar
+ * @param {number} escalar Nuemro por el que multiplicamos la matriz
+ * @returns Nueva matriz de numeros
+ */
+/**
+ * @author: Raisa Julia Ochoa Domínguez
+ *@description: Escribe una función llamada multiplicarMatrizPorEscalar que tome una matriz matriz y un número escalar.
+ *La función debe multiplicar cada elemento de la matriz por el escalar y devolver una nueva matriz con los
+ *resultados.
+ */
+
+/**
+ * Función con la que tomaremos una matriz y numero escalar que utilizaremos para multiplicar la matriz
+ * @param {[Array]} matriz Matriz de numeros a multiplicar
  * @param {number} escalar Nuemro por el que multiplicamos la matriz
  * @returns Nueva matriz de numeros
  */
 function multiplicarMatrizPorEscalar(matriz, escalar) {
+  if (typeof escalar !== "number") {
+    return "El escalar introducido no es un número válido";
+  }
+
   const resultado = [];
 
   for (let i = 0; i < matriz.length; i++) {
-    const filaResultado = [];
-    for (let j = 0; j < matriz[i].length; j++) {
-      filaResultado.push(matriz[i][j] * escalar);
-    }
+    if (!Array.isArray(matriz[i])) {
+      return `El elemento en la fila ${i} no es un array válido`;
+    } else {
+      const filaResultado = [];
+      for (let j = 0; j < matriz[i].length; j++) {
+        if (typeof matriz[i][j] !== "number") {
+          return `El elemento en la fila ${i} y columna ${j} no es un número válido`;
+        }
+        filaResultado.push(matriz[i][j] * escalar);
+      }
 
-    resultado.push(filaResultado);
+      resultado.push(filaResultado);
+    }
   }
 
   return resultado;
